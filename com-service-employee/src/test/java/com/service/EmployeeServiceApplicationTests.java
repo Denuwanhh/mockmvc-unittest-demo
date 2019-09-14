2062fa68a95ab859af9e3620fe090cb1a5f7553c
Denuwan Himanga Hettiarachi <denuwanonline@gmail.com>
2019-09-14 22:54:31 +0530

    Add Profiles

--- com-service-employee/src/test/java/com/service/EmployeeServiceApplicationTests.java
+++ com-service-employee/src/test/java/com/service/EmployeeServiceApplicationTests.java
@@ -1,12 +1,13 @@
 package com.service;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,11 +17,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.service.entity.Employee;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@Profile("dev")
 public class EmployeeServiceApplicationTests {
 
 	@Autowired
